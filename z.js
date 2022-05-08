11d20d5911f55b8867e7bce47ad2bd43b3163b6c
Yasein Burqan <mr.yasein@gmail.com>
2022-05-07 15:01:32 -0900

    Movies-Library

--- z.js
+++ z.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
-const moviesData = require("./movie-data/data.json");
+const moviesData = require("./Move Data/data.json");
 const url = "postgres://yaseinburqan:6437@localhost:5432/moviedatabase";
 const bodyParser = require("body-parser");
 
@@ -38,37 +38,24 @@ let handleAddMovie = (req, res) => {
   client
     .query(sql, values)
     .then((result) => {
-      // console.log(result.rows);
-      return res.status(201).json(result.rows[0]);
-    })
-    .catch();
-};
-
-let handleUpdateMovie = (req, res) => {
-  const { name, time, summary, image } = req.body;
-
-  let sql = "INSERT INTO movie(name,time,summary,image ) VALUES($1, $2, $3, $4) RETURNING *;"; // sql query
-  let values = [name, time, summary, image];
-  client
-    .query(sql, values)
-    .then((result) => {
-      // console.log(result.rows);
+      console.log(result.rows);
       return res.status(201).json(result.rows[0]);
     })
     .catch();
 };
 
 let handleHomePage = (req, res) => {
-  /* let sql = "SELECT * from movie;";
+  let sql = "SELECT * from movie;";
   client
     .query(sql)
     .then((result) => {
-      // console.log(result);
-      return res.status(200).json(result.rows);
+      console.log(result);
+      res.json(result.rows);
     })
     .catch((err) => {
       handleError(err, req, res);
-    }); */
+    });
+  return res.status(200).json(moviesLibrary);
 };
 
 let handleFavoritePage = (req, res) => {
@@ -83,4 +70,3 @@ app.post("/movie", handleAddMovie);
 app.get("/home", handleHomePage);
 app.get("/favorite", handleFavoritePage);
 app.get("*", handleError);
-app.put("/update-movie", handleUpdateMovie);
