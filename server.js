519f8a61dcdaf389b9c28c8fc521fadbf9dc4072
Yasein Burqan <76644013+YaseinBurqan@users.noreply.github.com>
2022-05-12 00:00:37 -0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 
 const moviesData = require("./movie-data/data.json");
-const url = "postgres://yaseinburqan:6437@localhost:5432/moviedatabase";
+const url = "process.env.dbUrl";
 const bodyParser = require("body-parser");
 
 const { Client } = require("pg");
@@ -15,72 +15,58 @@ const client = new Client(url);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+function handleListen(port) {}
+
 function MoviesLibrary(title, posterPath, overview) {
   this.title = title;
   this.posterPath = posterPath;
   this.overview = overview;
 }
 
-app.use(cors());
+app.get("/home", handleHomePage);
+app.get("/favorite", handleFavoritePage);
+app.get("/movie", handleMovie);
+app.get("/getMovieById/:id", handleGetMovieById);
+app.get("/trending", trendingPageHandler);
+app.get("*", handleError);
 
-// after connection to db, start the server
-client.connect().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening ${PORT}`);
-  });
+app.listen(3000, () => {
+  console.log("listening to port 3000");
 });
 
-let handleAddMovie = (req, res) => {
-  const { name, time, summary, image } = req.body;
-
-  let sql = "INSERT INTO movie(name,time,summary,image ) VALUES($1, $2, $3, $4) RETURNING *;"; // sql query
-  let values = [name, time, summary, image];
-  client
-    .query(sql, values)
-    .then((result) => {
-      // console.log(result.rows);
-      return res.status(201).json(result.rows[0]);
-    })
-    .catch();
-};
-
-let handleUpdateMovie = (req, res) => {
-  const { name, time, summary, image } = req.body;
-
-  let sql = "INSERT INTO movie(name,time,summary,image ) VALUES($1, $2, $3, $4) RETURNING *;"; // sql query
-  let values = [name, time, summary, image];
-  client
-    .query(sql, values)
-    .then((result) => {
-      // console.log(result.rows);
-      return res.status(201).json(result.rows[0]);
-    })
-    .catch();
-};
-
 let handleHomePage = (req, res) => {
-  /* let sql = "SELECT * from movie;";
-  client
-    .query(sql)
-    .then((result) => {
-      // console.log(result);
-      return res.status(200).json(result.rows);
-    })
-    .catch((err) => {
-      handleError(err, req, res);
-    }); */
+  return res.status(500).json(moviesLibrary);
 };
 
 let handleFavoritePage = (req, res) => {
-  return res.status(200).send("Favorite Page");
+  return res.status(500).send("Favorite Page");
 };
+
+let handleMovie = (req, res) => {
+  let newMovie = new MoviesLibrary(newMovie.title, newMovie.posterPath, newMovie.overview);
+  moviesLibrary.push(newMovie);
+  return res.status(500).send(newMovie);
+};
+
+function trendingPageHandler(req, res) {
+  let trendingMovies = [];
+  axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.apiKey}&language=en-US`).then((value) => {
+    value.data.results.forEach((movie) => {
+      movie = new MoviesLibrary(movie.title, movie.poster_path, movie.overview);
+      trendingMovies.push(movie);
+    });
+    return res.status(200).json(trendingMovies);
+  });
+}
+
+function handleGetMovieById(req, res) {
+  let id = req.params.id;
+  let sql = `SELECT * FROM movie WHERE id=${id};`;
+  client.query(sql).then((result) => {
+    res.status(200).json(result.rows);
+  });
+}
 
 const handleError = (req, res) => {
   return res.status(404).send("page not found");
 };
-
-app.post("/movie", handleAddMovie);
-app.get("/home", handleHomePage);
-app.get("/favorite", handleFavoritePage);
-app.get("*", handleError);
-app.put("/update-movie", handleUpdateMovie);
